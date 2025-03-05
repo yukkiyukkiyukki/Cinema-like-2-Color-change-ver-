e5bac2e61353feded57c4ef336bf776cadc1ad11
yukkiyukkiyukki <012700012016db8t@gl.pen-kanagawa.ed.jp>
2025-03-05 15:01:46 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+document.getElementById("fileInput").addEventListener("change", handleFile);
+document.getElementById("downloadBtn").addEventListener("click", downloadImage);
+
+let colorTable = {};
+fetch('color_table.json')
+    .then(response => response.json())
+    .then(data => colorTable = data);
+
+function handleFile(event) {
+    const file = event.target.files[0];
+    if (!file) return;
+
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+
+    if (file.type.startsWith("image/")) {
+        const img = new Image();
+        img.onload = () => {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img, 0, 0);
+            applyColorTransform(ctx, canvas.width, canvas.height);
+        };
+        img.src = URL.createObjectURL(file);
+    } else if (file.type.startsWith("video/")) {
+        const video = document.getElementById("video");
+        video.src = URL.createObjectURL(file);
+        video.hidden = false;
+        video.play();
+
+        video.onloadeddata = () => {
+            canvas.width = video.videoWidth;
+            canvas.height = video.videoHeight;
+            drawVideoFrame(video, ctx, canvas);
+        };
+    }
+}
+
+function drawVideoFrame(video, ctx, canvas) {
+    if (video.paused || video.ended) return;
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    applyColorTransform(ctx, canvas.width, canvas.height);
+    requestAnimationFrame(() => drawVideoFrame(video, ctx, canvas));
+}
+
+function applyColorTransform(ctx, width, height) {
+    let imageData = ctx.getImageData(0, 0, width, height);
+    let data = imageData.data;
+
+    for (let i = 0; i < data.length; i += 4) {
+        let r = data[i], g = data[i+1], b = data[i+2];
+        let key = `${r},${g},${b}`;
+
+        if (colorTable[key]) {
+            let [newR, newG, newB] = colorTable[key].split(',').map(Number);
+            data[i] = newR;
+            data[i+1] = newG;
+            data[i+2] = newB;
+        } else {
+            let nearestColor = findNearestColor(r, g, b);
+            data[i] = nearestColor[0];
+            data[i+1] = nearestColor[1];
+            data[i+2] = nearestColor[2];
+        }
+    }
+
+    ctx.putImageData(imageData, 0, 0);
+}
+
+function findNearestColor(r, g, b) {
+    let minDistance = Infinity;
+    let closestColor = [r, g, b];
+
+    for (let key in colorTable) {
+        let [cr, cg, cb] = key.split(',').map(Number);
+        let distance = Math.sqrt((r - cr) ** 2 + (g - cg) ** 2 + (b - cb) ** 2);
+
+        if (distance < minDistance) {
+            minDistance = distance;
+            closestColor = colorTable[key].split(',').map(Number);
+        }
+    }
+
+    return closestColor;
+}
+
+function downloadImage() {
+    const canvas = document.getElementById("canvas");
+    const link = document.createElement("a");
+    link.download = "converted_image.png";
+    link.href = canvas.toDataURL("image/png");
+    link.click();
+}
